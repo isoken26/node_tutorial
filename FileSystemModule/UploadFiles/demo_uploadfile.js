b1b460b65c10a2bf3d06693538e6daeab9f04249
0224009 磯村 建司郎 <kenjiro_isomura@mjs.co.jp>
2019-01-22 13:22:37 +0900

    file upload parse file

--- FileSystemModule/UploadFiles/demo_uploadfile.js
+++ FileSystemModule/UploadFiles/demo_uploadfile.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const formidable = require("formidable");
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+    if (req.url === '/fileupload') {
+        const form = new formidable.IncomingForm();
+
+        form.parse(req, (err, fields, files) => {
+            res.write('File uploaded');
+            res.end();
+        });
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
 }).listen(8080);
